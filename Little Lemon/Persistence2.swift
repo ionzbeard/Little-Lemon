044ae5eb0865b596694ec995d8e630c2d608c95c
jonathan cabral <05jcabral@gmail.com>
2024-04-17 23:31:38 -0400

    Update UserInterfaceState.xcuserstate

---
+++ Little Lemon/Persistence2.swift
@@ -0,0 +1,22 @@
+import CoreData
+import Foundation
+
+struct PersistenceController2 {
+    static let shared = PersistenceController2()
+
+    let container: NSPersistentContainer
+
+    init() {
+        container = NSPersistentContainer(name: "ExampleDatabase")
+        container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        container.loadPersistentStores(completionHandler: {_,_ in })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    func clear() {
+        // Delete all dishes from the store
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Dish")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        let _ = try? container.persistentStoreCoordinator.execute(deleteRequest, with: container.viewContext)
+    }
+}
